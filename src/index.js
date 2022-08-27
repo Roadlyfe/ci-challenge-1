13f37ec1d192d5172c42070fdb6ba240a9adfc92
Adam Smith <adamsmith@Adams-MacBook-Air.local>
2022-08-27 13:19:38 -0500

    finished project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,226 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const NEXT_HTML = ` 
+<section class="card">
+  <div>
+      <div>
+          <h2>Next-generation threat detection</h2>
+      </div>
+      <div class="card-p">
+          <p>TAP is built on our next-generation email security and cloud platforms to give you clear visibility
+              into all communications and files in your SaaS file stores. Deep user-level and message-level
+              context across email and SaaS makes TAP especially effective at identifying hard-to-catch threats
+              that other solutions miss.
+          </p>
+      </div>
+      <div class="bullet">
+          <ul>
+              <li>Block and quarentine messages with malicious attachments or URLs</li>
+              <li>Use cloud based scanning to detect and inspect malicious content</li>
+              <li>Rewrite embedded URLs to protect users on any device or network</li>
+          </ul>
+          <ul>
+              <li>Detect ransomeware and malicious files in SaaS file stores</li>
+              <li>Track and block clicks to malicious webpages</li>
+              <li>Surface acocount compromise from brute-force attacks</li>
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+          </ul>
+      </div>
+      <div class="bordertop">
+          <h3>
+              <span>Download the data sheet</span>
+              <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" clip-rule="evenodd"
+                      d="M0.625558 0.304241C0.312405 0.617615 0.312405 1.1229 0.625308 1.43602L5.06303 5.87375L0.625427 10.3115C0.31235 10.6245 0.31235 11.1301 0.625474 11.4432C0.775923 11.5936 0.980366 11.6785 1.19023 11.6785C1.40258 11.6785 1.60996 11.594 1.76171 11.4362L6.75963 6.43857C7.07231 6.13167 7.07412 5.62138 6.7618 5.30906L1.75722 0.30437C1.44397 -0.0086616 0.938681 -0.00866161 0.625558 0.304241ZM5.91127 5.87274L6.05469 6.01616C5.9752 5.93668 5.97472 5.81075 6.05304 5.73097L5.91127 5.87274Z"
+                      fill="#009ADA" />
+              </svg>
+          </h3>
+      </div>
+  </div>
+</section>`
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const SOPHISTICATED_HTML = `
+ <section class="card">
+ <div>
+     <div>
+         <h2>Sophisticated threat analysis</h2>
+     </div>
+     <div class="card-p">
+         <p>Doggo ipsum i am bekom fat the neighborhood pupper very hand that feed shibe. Yapper big ol
+             pupper. visibility into all communications and files in your SaaS file stores. Deep
+             user-level and message-level context across email and SaaS makes TAP especially effective
+             at identifying hard-to-catch threats that other solutions miss.
+         </p>
+     </div>
+     <div class="bullet">
+         <ul>
+             <li>Smol borkf most angery pupper I have ever seen heckin angry woofer</li>
+             <li>the neighborhood pupper. Lotsa pats pupperino corgo pats</li>
+             <li>I am bekom fat noodle horse you are doin me a concern</li>
+         </ul>
+         <ul>
+             <li>Smol borking doggo with a long snoot for pats lotsa pats </li>
+             <li>long bois doge very jealous pupper, heck heckin good boys ruff</li>
+             <li>He made many woofs the neighborhood pupper heckin angery woofer</li>
+         </ul>
+     </div>
+     <div class="bordertop">
+         <h3>
+             <span>Download the data sheet</span>
+             <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                 <path fill-rule="evenodd" clip-rule="evenodd"
+                     d="M0.625558 0.304241C0.312405 0.617615 0.312405 1.1229 0.625308 1.43602L5.06303 5.87375L0.625427 10.3115C0.31235 10.6245 0.31235 11.1301 0.625474 11.4432C0.775923 11.5936 0.980366 11.6785 1.19023 11.6785C1.40258 11.6785 1.60996 11.594 1.76171 11.4362L6.75963 6.43857C7.07231 6.13167 7.07412 5.62138 6.7618 5.30906L1.75722 0.30437C1.44397 -0.0086616 0.938681 -0.00866161 0.625558 0.304241ZM5.91127 5.87274L6.05469 6.01616C5.9752 5.93668 5.97472 5.81075 6.05304 5.73097L5.91127 5.87274Z"
+                     fill="#009ADA" />
+             </svg>
+         </h3>
+     </div>
+ </div>
+</section>`
+
+const PROTECTION_HTML = `
+ <section class="card">
+  <div>
+      <div>
+          <h2>Protection beyond the network</h2>
+      </div>
+      <div class="card-p">
+          <p>Doggo ipsum i am bekom fat the neighborhood pupper very hand that feed shibe. Yapper big ol
+              pupper. visibility into all communications and files in your SaaS file stores. Deep
+              user-level and message-level context across email and SaaS makes TAP especially effective
+              at identifying hard-to-catch threats that other solutions miss.
+          </p>
+      </div>
+      <div class="bullet">
+          <ul>
+              <li>Smol borkf most angery pupper I have ever seen heckin angry woofer</li>
+              <li>the neighborhood pupper. Lotsa pats pupperino corgo pats</li>
+              <li>I am bekom fat noodle horse you are doin me a concern</li>
+          </ul>
+          <ul>
+              <li>Smol borking doggo with a long snoot for pats lotsa pats </li>
+              <li>long bois doge very jealous pupper, heck heckin good boys ruff</li>
+              <li>He made many woofs the neighborhood pupper heckin angery woofer</li>
+          </ul>
+      </div>
+      <div class="bordertop">
+          <h3>
+              <span>Download the data sheet</span>
+              <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" clip-rule="evenodd"
+                      d="M0.625558 0.304241C0.312405 0.617615 0.312405 1.1229 0.625308 1.43602L5.06303 5.87375L0.625427 10.3115C0.31235 10.6245 0.31235 11.1301 0.625474 11.4432C0.775923 11.5936 0.980366 11.6785 1.19023 11.6785C1.40258 11.6785 1.60996 11.594 1.76171 11.4362L6.75963 6.43857C7.07231 6.13167 7.07412 5.62138 6.7618 5.30906L1.75722 0.30437C1.44397 -0.0086616 0.938681 -0.00866161 0.625558 0.304241ZM5.91127 5.87274L6.05469 6.01616C5.9752 5.93668 5.97472 5.81075 6.05304 5.73097L5.91127 5.87274Z"
+                      fill="#009ADA" />
+              </svg>
+          </h3>
+      </div>
+  </div>
+</section>`
+
+const ATTACK_HTML = `
+   <section class="card" >
+  <div>
+    <div>
+        <h2>TAP attack index</h2>
+    </div>
+    <div class="card-p">
+        <p>Doggo ipsum i am bekom fat the neighborhood pupper very hand that feed shibe. Yapper big ol
+            pupper. visibility into all communications and files in your SaaS file stores. Deep
+            user-level and message-level context across email and SaaS makes TAP especially effective
+            at identifying hard-to-catch threats that other solutions miss.
+        </p>
+    </div>
+    <div class="bullet">
+        <ul>
+            <li>Smol borking doggo with a long snoot for pats lotsa pats </li>
+            <li>long bois doge very jealous pupper, heck heckin good boys ruff</li>
+            <li>He made many woofs the neighborhood pupper heckin woofer</li>
+        </ul>
+        <ul>
+            <li>Smol borkf most angery pupper I have ever seen heckin angry woofer</li>
+            <li>the neighborhood pupper. Lotsa pats pupperino corgo pats</li>
+            <li>I am bekom fat noodle horse you are doin me a concern</li>
+        </ul>
+    </div>
+    <div class="bordertop">
+        <h3>
+            <span>Download the data sheet</span>
+            <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd"
+                    d="M0.625558 0.304241C0.312405 0.617615 0.312405 1.1229 0.625308 1.43602L5.06303 5.87375L0.625427 10.3115C0.31235 10.6245 0.31235 11.1301 0.625474 11.4432C0.775923 11.5936 0.980366 11.6785 1.19023 11.6785C1.40258 11.6785 1.60996 11.594 1.76171 11.4362L6.75963 6.43857C7.07231 6.13167 7.07412 5.62138 6.7618 5.30906L1.75722 0.30437C1.44397 -0.0086616 0.938681 -0.00866161 0.625558 0.304241ZM5.91127 5.87274L6.05469 6.01616C5.9752 5.93668 5.97472 5.81075 6.05304 5.73097L5.91127 5.87274Z"
+                    fill="#009ADA" />
+            </svg>
+        </h3>
+    </div>
+ </div>
+</section>`
+
+const DASHBOARD_HTML = `
+  <section class="card">
+  <div>
+      <div>
+          <h2>TAP dashboard</h2>
+      </div>
+      <div class="card-p">
+          <p>Doggo ipsum i am bekom fat the neighborhood pupper very hand that feed shibe. Yapper big ol
+              pupper. visibility into all communications and files in your SaaS file stores. Deep
+              user-level and message-level context across email and SaaS makes TAP especially effective
+              at identifying hard-to-catch threats that other solutions miss.
+          </p>
+      </div>
+      <div class="bullet">
+          <ul>
+              <li>Smol borkf most angery pupper I have ever seen heckin angry woofer</li>
+              <li>the neighborhood pupper. Lotsa pats pupperino corgo pats</li>
+              <li>I am bekom fat noodle horse you are doin me a concern</li>
+          </ul>
+          <ul>
+              <li>Smol borking doggo with a long snoot for pats lotsa pats </li>
+              <li>long bois doge very jealous pupper, heck heckin good boys ruff</li>
+              <li>He made many woofs the neighborhood pupper heckin angery woofer</li>
+          </ul>
+      </div>
+      <div class="bordertop">
+          <h3>
+              <span>Download the data sheet</span>
+              <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" clip-rule="evenodd"
+                      d="M0.625558 0.304241C0.312405 0.617615 0.312405 1.1229 0.625308 1.43602L5.06303 5.87375L0.625427 10.3115C0.31235 10.6245 0.31235 11.1301 0.625474 11.4432C0.775923 11.5936 0.980366 11.6785 1.19023 11.6785C1.40258 11.6785 1.60996 11.594 1.76171 11.4362L6.75963 6.43857C7.07231 6.13167 7.07412 5.62138 6.7618 5.30906L1.75722 0.30437C1.44397 -0.0086616 0.938681 -0.00866161 0.625558 0.304241ZM5.91127 5.87274L6.05469 6.01616C5.9752 5.93668 5.97472 5.81075 6.05304 5.73097L5.91127 5.87274Z"
+                      fill="#009ADA" />
+              </svg>
+          </h3>
+      </div>
+  </div>
+</section>`
+
+var render = function (text, node) {
+  node.innerHTML = text;
+};
+
+document.getElementById('next').addEventListener('click', function () {
+  render(NEXT_HTML, document.querySelector('#main'));
+});
+
+document.getElementById('sophisticated').addEventListener('click', function () {
+  render(SOPHISTICATED_HTML, document.querySelector('#main'));
+});
+
+document.getElementById('protection').addEventListener('click', function () {
+  render(PROTECTION_HTML, document.querySelector('#main'));
+});
+
+document.getElementById('attack').addEventListener('click', function () {
+  render(ATTACK_HTML, document.querySelector('#main'));
+});
+document.getElementById('dashboard').addEventListener('click', function () {
+  render(DASHBOARD_HTML, document.querySelector('#main'));
+});
+
+window.addEventListener("resize", function () {
+  const ALL_HTML = NEXT_HTML + SOPHISTICATED_HTML + PROTECTION_HTML + ATTACK_HTML + DASHBOARD_HTML;
+  
+  if (!window.matchMedia("(min-width: 768px)").matches) {
+    render(ALL_HTML, document.querySelector('#main'));
+  } else {
+    render(NEXT_HTML, document.querySelector('#main'));
+  }
+});
+
+render(NEXT_HTML, document.querySelector('#main'));
+
